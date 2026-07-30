c600ee2a063ea541517731da799640eb71bf69f2
Gemma Maeso <gemmaa.dev@gmail.com>
2026-07-30 10:47:17 +0200

    feat: extract ProductCard into its own component and add handleCardClick

--- src/features/product/components/ProductCard.tsx
+++ src/features/product/components/ProductCard.tsx
@@ -1,3 +1,23 @@
-export function ProductCard() {
-  return <h1>Product Card</h1>;
+import type { Product } from "../types/product";
+
+interface ProductCardProps {
+  product: Product;
+  onClick: (id: string) => void;
+}
+
+export function ProductCard({ product, onClick }: ProductCardProps) {
+  return (
+    <li
+      className="flex flex-col boder shadow-sm border-gray-200 p-6 rounded gap-2"
+      onClick={() => onClick(product.id)}
+    >
+      <img
+        src={product.image.src}
+        alt={product.image.alt}
+        className="w-full h-40 object-cover"
+      />
+      <h3 className="text-xl font-bold">{product.title}</h3>
+      <p className="text-xl">{product.price}€</p>
+    </li>
+  );
 }
--- src/features/product/components/ProductList.tsx
+++ src/features/product/components/ProductList.tsx
@@ -1,22 +1,26 @@
 import { useState } from "react";
 import { products } from "../../../data/products";
 import type { Product } from "../types/product";
+import { ProductCard } from "./ProductCard";
+import { useNavigate } from "react-router";
 
 export function ProductList() {
   const [productList] = useState<Product[]>(products);
 
+  const navigate = useNavigate();
+
+  const handleCardClick = (id: string) => {
+    navigate(`/products/${id}`);
+  };
+
   return (
     <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5">
       {productList.map((product) => (
-        <li className="flex flex-col boder shadow-sm border-gray-200 p-6 rounded gap-2">
-          <img
-            src={product.image.src}
-            alt={product.image.alt}
-            className="w-full h-40 object-cover"
-          />
-          <h3 className="text-xl font-bold">{product.title}</h3>
-          <p className="text-xl">{product.price}€</p>
-        </li>
+        <ProductCard
+          key={product.id}
+          product={product}
+          onClick={handleCardClick}
+        />
       ))}
     </ul>
   );
