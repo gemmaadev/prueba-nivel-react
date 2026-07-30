864d35dab779d80e85176ddb8cd33aef3ca2b9ad
Gemma Maeso <gemmaa.dev@gmail.com>
2026-07-30 10:17:58 +0200

    feat(NotFound): style NotFound page (super basic) with navigation button

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router";
 export default function NotFound() {
-  return <h1>Not Found</h1>;
+  return (
+    <div className="flex flex-1 flex-col justify-center text-center items-center gap-3">
+      <h1 className="font-bold text-4xl">Error 404 - Página no encontrada</h1>
+      <p className="text-xl">
+        Lo sentimos, la página a la que intentas acceder no existe o no se
+        encuentra disponible
+      </p>
+      <Link
+        to={"/products"}
+        className="border text-xl bg-blue-700 text-white rounded px-4 py-2"
+      >
+        Volver a la tienda
+      </Link>
+    </div>
+  );
 }
