fd139bf3f23613e89ef4d948b8f56f5c233a1d47
Gemma Maeso <gemmaa.dev@gmail.com>
2026-07-30 10:16:29 +0200

    feat(HomePage): style HomePage(super basic) with navigation button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,3 +1,20 @@
+import { Link } from "react-router";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  return (
+    <div className="flex flex-1 flex-col justify-center text-center items-center gap-3">
+      <h1 className="font-bold text-4xl">
+        Bienvenido/a a la tienda tech online!
+      </h1>
+      <p className="text-xl">
+        Descubre todos nuestros productos de última generación
+      </p>
+      <Link
+        to={"/products"}
+        className="border text-xl bg-blue-700 text-white rounded px-4 py-2"
+      >
+        Ir a la tienda
+      </Link>
+    </div>
+  );
 }
